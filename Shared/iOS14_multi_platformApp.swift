6e11eb4d16162cc2f8e04d85427f4c333a0a5d0c
KAMONPAN KAEWHEANG <kamonpan@dit.daikin.co.jp>
2022-01-23 23:44:57 +0700

    Initial Commit

---
+++ Shared/iOS14_multi_platformApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS14_multi_platformApp.swift
+//  Shared
+//
+//  Created by admin on 23/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS14_multi_platformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
