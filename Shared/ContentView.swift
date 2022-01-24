478b2b639946d6796aebfab527a6600a80a1f962
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-01-24 20:57:49 +0700

    Adding SwiftUI Visual Editor

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            
+            HStack {
+                HStack {
+                    Spacer()
+                    Image("Illustration 1")
+                        //use all available space
+                        .resizable()
+                        //make images fit on screen size
+                        .aspectRatio(contentMode: .fit)
+                }
+                Spacer()
+            }
+            
+            Text("SwiftUI for iOS 14")
+                .font(.body)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+            
+            Text("add content")
+                .font(.footnote)
+                .foregroundColor(Color.white)
+        }
+        .padding(.all)
+        .background(Color.blue)
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        .cornerRadius(20.0)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/))
+            .preferredColorScheme(.dark)
     }
 }
