7a0364f92cb2d5ab82fd22ac560cf5276e44b139
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-01-24 21:12:25 +0700

    Adding SF Symbols and List Styles

---
+++ Shared/CourseRow.swift
@@ -0,0 +1,40 @@
+//
+//  CourseRow.swift
+//  iOS14-multi-platform
+//
+//  Created by admin on 24/1/2565 BE.
+//
+
+import SwiftUI
+
+struct CourseRow: View {
+    var body: some View {
+        HStack(alignment: .top) {
+            Image(systemName: "paperplane.circle.fill")
+                //adding renderingMode to get color
+                
+                .renderingMode(.template)
+                .frame(width: 48.0, height: 48.0)
+                .imageScale(.medium)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.white)
+            
+            VStack(alignment: .leading, spacing: 4.0) {
+                Text("SwiftUI")
+                    .font(.subheadline).bold()
+                Text("Description")
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct CourseRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseRow()
+    }
+}
---
+++ Shared/CoursesView.swift
@@ -0,0 +1,23 @@
+//
+//  CoursesView.swift
+//  iOS14-multi-platform
+//
+//  Created by admin on 24/1/2565 BE.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        List(0 ..< 20) { item in
+            CourseRow()
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
