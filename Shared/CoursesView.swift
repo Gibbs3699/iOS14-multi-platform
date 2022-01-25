127b5386209b99beec277b8c3b0f9335864ae11d
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-01-25 22:46:23 +0700

    Adding Show View and Transitions

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct CoursesView: View {
+    @State var show = false
     var body: some View {
-        Text("")
+        ZStack {
+            CourseItem()
+                .frame(width: 335, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            VStack{
+                if show {
+                    CourseItem()
+                        .transition(.move(edge: .trailing))
+                        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        //bring to front
+//                        .zIndex(1)
+                }
+            }
+        }
+        .onTapGesture {
+            withAnimation(.spring()) {
+                show.toggle()
+            }
+        }
+        // simple fade in/out
+//        .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+//        .animation(.spring())
     }
 }
 
