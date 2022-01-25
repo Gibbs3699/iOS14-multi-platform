b05ee7bfc8cbb81dbf7009fcd0f1caeca365c4b5
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-01-25 22:55:01 +0700

    Adding Matched Geometry Effect

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -9,28 +9,41 @@ import SwiftUI
 
 struct CoursesView: View {
     @State var show = false
+    @Namespace var namespace
     var body: some View {
         ZStack {
             CourseItem()
+                //matchedGeometryEffect only on iOS 14
+                //share animate between 2 views
+                .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
                 .frame(width: 335, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            VStack{
                 if show {
-                    CourseItem()
-                        .transition(.move(edge: .trailing))
-                        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    ScrollView {
+                        CourseItem()
+                            .matchedGeometryEffect(id: "Card", in: namespace)
+                            .frame(height: 300)
+                        VStack {
+                            ForEach(0 ..< 20) { item in
+                                CourseRow()
+                            }
+                        }
+                        .padding()
+                    }
+                    .transition(.opacity)
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                         //bring to front
 //                        .zIndex(1)
-                }
             }
         }
         .onTapGesture {
+            //adding withAnimation won't lag
             withAnimation(.spring()) {
                 show.toggle()
             }
         }
+        //adding animation will lag
         // simple fade in/out
 //        .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
-//        .animation(.spring())
     }
 }
 
